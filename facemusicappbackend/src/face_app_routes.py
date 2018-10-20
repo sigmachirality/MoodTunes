77d6345f710ff520752990561ca92a8de017e8c1
Daniel Tao <danieltaox@gmail.com>
2018-10-20 15:21:01 -0400

    Add function to convert from emotion to spotify values

--- facemusicappbackend/src/face_app_routes.py
+++ facemusicappbackend/src/face_app_routes.py
@@ -6,6 +6,7 @@ import base64
 import re
 import io
 import json
+import random
 
 app = Flask(__name__)
 
@@ -83,7 +84,72 @@ Analyzes a emotion dictionary for emotions
 def analyze_emotion(emotion):
     inverse = [(value, key) for key, value in emotion.items()]
     return max(inverse)[1]
-    
+
+"""
+Get spotify parameters for specific emotion
+"""
+def emotion_to_spotify(emotion):
+    if emotion == "Anger" or emotion == "Contempt":
+        spotifyData = {
+                "Energy" : random.uniform(0.2,0.5),
+                "Valence" : random.uniform(0.6,0.8),
+                "Tempo" : random.uniform(0.0,0.3),
+                "Loudness" : random.uniform(0.3,0.5),
+                "Danceability" : random.uniform(0.1,0.6)  
+            }
+    elif emotion == "Disgust":
+        spotifyData = {
+                "Energy" : random.uniform(0.2,0.6),
+                "Valence" : random.uniform(0.8,1.0),
+                "Tempo" : random.uniform(0.3,0.5),
+                "Loudness" : random.uniform(0.2,0.5),
+                "Danceability" : random.uniform(0.2,0.5)  
+            }
+    elif emotion == "Fear":
+        spotifyData = {
+                "Energy" : random.uniform(0.1,0.4),
+                "Valence" : random.uniform(0.0,1.0),
+                "Tempo" : random.uniform(0.0,0.3),
+                "Loudness" : random.uniform(0.1,0.3),
+                "Danceability" : random.uniform(0.1,0.5)  
+            }
+    elif emotion == "Happiness":
+        spotifyData = {
+                "Energy" : random.uniform(0.8,1.0),
+                "Valence" : random.uniform(0.0,1.0),
+                "Tempo" : random.uniform(0.5,1.0),
+                "Loudness" : random.uniform(0.4,1.0),
+                "Danceability" : random.uniform(0.7,1.0)  
+            }
+    elif emotion == "Neutral":
+        spotifyData = {
+                "Energy" : random.uniform(0.6,0.9),
+                "Valence" : random.uniform(0.5,1.0),
+                "Tempo" : random.uniform(0.4,0.8),
+                "Loudness" : random.uniform(0.5,0.8),
+                "Danceability" : random.uniform(0.5,0.9)  
+            }
+    elif emotion == "Surprise":
+        spotifyData = {
+                "Energy" : random.uniform(0.4,0.8),
+                "Valence" : random.uniform(0.1,1.0),
+                "Tempo" : random.uniform(0.5,0.7),
+                "Loudness" : random.uniform(0.4,0.7),
+                "Danceability" : random.uniform(0.4,0.8)  
+            }
+    elif emotion == "Sadness":
+        spotifyData = {
+                "Energy" : random.uniform(0.2,0.6),
+                "Valence" : random.uniform(0.1,1.0),
+                "Tempo" : random.uniform(0.1,0.5),
+                "Loudness" : random.uniform(0.2,0.6),
+                "Danceability" : random.uniform(0.3,0.4)  
+            }
+    else:
+        spotifyData = {}
+    spotifyData['Tempo'] = spotifyData['Tempo'] * 180
+    spotifyData['Loudness'] = spotifyData['Loudness'] * -70
+    return spotifyData
 
 
 """
